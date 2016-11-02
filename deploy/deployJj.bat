9c6fea37ec763632e75d164a6b5ce8516a9dd7f0
Dave Badia <dbadiaoss@gmail.com>
2016-11-01 20:22:06 -0400

    fix script

--- deploy/deployJj.bat
+++ deploy/deployJj.bat
@@ -1,6 +1,6 @@
 REM @echo off
 
-putty.exe -ssh tomcat7@jj -m deploy\moveOldWarJj.txt || goto :error
+putty.exe -ssh tomcat7@jj -m moveOldWarJj.txt || goto :error
 pscp target\sqrlexample.war tomcat7@jj:/var/lib/tomcat7/webapps
 
 exit /b 0
